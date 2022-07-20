d4040f2bfccd255c4e591a27cf01006cdcb2f1f5
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-07-19 23:09:46 -0400

    media - cards border

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
-// import "./App.css";
-import { Cards } from "./components/cards";
-=======
-
 import { ProxVacinas } from './components/proxVacinas';
 
-
-import { NavBar } from "./components/navbar/index";
-import { Progress } from "./components/progress";
-import { Footer } from "./components/footer";
->>>>>>> 80d21e4f7bfe1bf481ffed1549e27f9ac5cee08f
+import { NavBar } from './components/navbar/index';
+import { Progress } from './components/progress';
+import { Footer } from './components/footer';
+import { CardsImg } from './components/cards';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <h2>eai mundao</h2>
-<<<<<<< HEAD
-      <h3>Tathy aqui</h3>
-      <Cards />
-=======
-      <Progress />
-
->>>>>>> 80d21e4f7bfe1bf481ffed1549e27f9ac5cee08f
-    </div>
-  );
-
+	return (
+		<div className="App">
+			<Progress />
+			<ProxVacinas />
+			<CardsImg />
+		</div>
+	);
 }
 
 export default App;
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,68 +1,46 @@
-<<<<<<< HEAD
-import hospital from '../../assets/images/imgproject/icon-hospital.png';
-export function Cards() {
+import hospital from '../..//assets/images/imgproject/icon-hospital.png';
+import heart from '../..//assets/images/imgproject/health-icon.png';
+import needle from '../../assets/images/imgproject/injecao-icon.png';
+import family from '../../assets/images/imgproject/people-icon.png';
+import style from './style.module.css';
+export function CardsImg() {
 	return (
-		<>
-			<div
-				className="card"
-				style={{
-					width: '18rem',
-					borderRadius: '12px',
-					backgroundColor: '#FCDAD9',
-				}}
-			>
-				<img className="card-img-top" src={hospital} alt="Card cap" />
-				<div className="card-body">
-					<p className="card-text">UBS</p>
+		<div className={style.scrollingWrapper}>
+			<div className={style.card} style={{ backgroundColor: '#FCDAD9' }}>
+				<div className={style.out}>
+					<img className={style.img} src={hospital} alt="ubs" />
+					<div className="card-body">
+						<p className="card-text">UBS proximas</p>
+					</div>
 				</div>
 			</div>
-		</>
+
+			<div className={style.card} style={{ backgroundColor: '#F9F0C2' }}>
+				<div className={style.out}>
+					<img className={style.img} src={heart} alt="campanhas" />
+					<div className="card-body">
+						<p className="card-text">Campanhas</p>
+					</div>
+				</div>
+			</div>
+
+			<div className={style.card} style={{ backgroundColor: '#96BEDA' }}>
+				<div className={style.out}>
+					<img className={style.img} src={needle} alt="vacinas" />
+					<div className="card-body">
+						<p className="card-text">Sobre Vacinas</p>
+					</div>
+				</div>
+			</div>
+
+			<div className={style.card} style={{ backgroundColor: '#FFBEBD' }}>
+				<div className={style.out}>
+					<img className={style.img} src={family} alt="faixa etaria" />
+					<div className="card-body">
+						<p className="card-text">Faixa etária</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
-=======
-import hospital from "../..//assets/images/imgproject/icon-hospital.png";
-import heart from "../..//assets/images/imgproject/health-icon.png";
-import needle from "../../assets/images/imgproject/injecao-icon.png";
-import family from "../../assets/images/imgproject/people-icon.png";
-import style from "./style.module.css";
-export function CardsImg() {
-  return (
-    <div className={style.scrollingWrapper}>
-      <div className={style.card} style={{ backgroundColor: "#FCDAD9" }}>
-        <div className={style.out}>
-          <img className={style.img} src={hospital} alt="ubs" />
-          <div className="card-body">
-            <p className="card-text">UBS proximas</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={style.card} style={{ backgroundColor: "#F9F0C2" }}>
-        <div className={style.out}>
-          <img className={style.img} src={heart} alt="campanhas" />
-          <div className="card-body">
-            <p className="card-text">Campanhas</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={style.card} style={{ backgroundColor: "#96BEDA" }}>
-        <div className={style.out}>
-          <img className={style.img} src={needle} alt="vacinas" />
-          <div className="card-body">
-            <p className="card-text">Sobre Vacinas</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={style.card} style={{ backgroundColor: "#FFBEBD" }}>
-        <div className={style.out}>
-          <img className={style.img} src={family} alt="faixa etaria" />
-          <div className="card-body">
-            <p className="card-text">Faixa etária</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
->>>>>>> 80d21e4f7bfe1bf481ffed1549e27f9ac5cee08f
 }
